044ed0e34ea1d4b1a8f3eebc5d4d13d329da26a9
Bill Barber <81barberw@gmail.com>
2019-01-07 19:28:22 -0600

    First commit

---
+++ src/app/message.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  messages: string[] = [];
+
+  add(messages: string) {
+    this.messages.push(messages);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+}
